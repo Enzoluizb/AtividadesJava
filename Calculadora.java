3c822612f1d07935e21fd829711e398cff5d8766
enzoluizb <enzoluiz.brt@gmail.com>
2024-04-22 19:29:56 -0300

    calculadora

---
+++ Calculadora.java
@@ -0,0 +1,38 @@
+public class Calculadora {
+
+    double memoria;
+    double v1;
+    double v2;
+
+    public void Guardar(double n) {
+        this.memoria = n;
+    }
+
+    public double Recuperar() {
+        return this.memoria;
+    }
+
+    public double Somar(
+            double v1,
+            double v2) {
+        return (v1 + v2);
+    }
+
+    public double Subtrair(
+            double v1,
+            double v2) {
+        return (v1 - v2);
+    }
+
+    public double Multiplicar(
+            double v1,
+            double v2) {
+        return (v1 * v2);
+    }
+
+    public double Dividir(
+            double v1,
+            double v2) {
+        return (v1 / v2);
+    }
+}
